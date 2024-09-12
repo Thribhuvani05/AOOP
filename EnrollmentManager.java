b160091bd7b5f6191c6fbcd2a89b30b8cd864e17
Thribhuvani05 <2320090044@klh.edu.in>
2024-09-12 10:43:10 +0530

    Delete EnrollmentManager.java

--- EnrollmentManager.java
+++
@@ -1,8 +0,0 @@
-package Com.Student_Info;
-
-public class EnrollmentManager {
-	public void enroll(Student student, Course course) {
-        course.enrollStudent(student);
-        System.out.println(student.getName() + " has been enrolled in " + course.getCourseName());
-    }
-}
